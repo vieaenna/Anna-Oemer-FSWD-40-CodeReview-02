7f6407975cc19a873090d7cc9413f07352ef11a4
vieaenna <38191144+vieaenna@users.noreply.github.com>
2018-04-21 15:29:28 +0200

    Add files via upload

---
+++ price.js
@@ -0,0 +1,10 @@
+var ProductOne = "Trip to Berlin";
+ProductOne = 680;
+var ProductTwo = "Trip to Prague";
+ProductTwo = 540;
+
+
+function addition(ProductOne,ProductTwo) {
+   var sum = ProductOne + ProductTwo;
+   document.write(sum);
+}
